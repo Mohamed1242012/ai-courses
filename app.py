e039fa73ba894d66eb2ae37bbc1a1e7c1df0272e
Mohamed Elsayed <mo212el4@gmail.com>
2025-04-19 05:36:03 +0400

    added ability for ai to create db entities based on user request

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request, Response
 import os
 from dotenv import load_dotenv
 import psycopg2
+from google import genai # Use gemeni api with this
+import json
 
 load_dotenv()
 
 app = Flask(__name__)
-url = os.getenv("POSTGRESQL_URL")
-connection = psycopg2.connect(url)
+DB_URL = os.getenv("POSTGRESQL_URL")
+GENAI_API = os.getenv("GENAI_API")
+connection = psycopg2.connect(DB_URL)
 
 with connection:
   with connection.cursor() as cursor:
@@ -41,6 +44,42 @@ CREATE TABLE IF NOT EXISTS "message" (
 
 """)
 
-@app.route("/")
+@app.route("/api/course",methods=["POST","DELETE","PATCH"])
 def home():
-  return "Hello World"
+  data = request.get_json()
+  # Sample data we get from client
+  {
+    "learn": "Python",
+    "notes": "I have some scratch experience."
+  }
+
+  if request.method == "POST": # POST for create
+    client = genai.Client(api_key=GENAI_API)
+    genai_response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=f"""
+You are used in an api for a learning platform, please generate some json data.
+You will get now the user input in two text boxes,
+The user wants to learn: [{data.get("learn")}]
+Please generate a small title and description of what the user needs to learn, your data will be used to create an entety in a database.
+Responde with the following schema example:
+{{
+  "title": "Python",
+  "description": "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation."
+}}
+""",
+        config={
+          'response_mime_type': 'application/json',
+        }
+    )
+    genai_response_obj = json.loads(genai_response.text)
+    with connection:
+      with connection.cursor() as cursor:
+        cursor.execute("""
+INSERT INTO "course" (title,description) VALUES
+	(
+  	%s,
+    %s
+  );
+""",(genai_response_obj.get("title"),genai_response_obj.get("description")))
+    return "", 202
